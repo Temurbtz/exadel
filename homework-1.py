e3f182f73f35c1d2e9707bf1e95b974bd4b74c74
Temurbekjonbtz <56353734+Temurbekjonbtz@users.noreply.github.com>
2022-04-29 12:49:36 +0500

    second  commit

--- homework-1.py
+++ homework-1.py
@@ -1,74 +1,66 @@
 import  json
+from context_manager  import File
 class  Converter:
-   
-   def csvtojson(self):
-    f=open("input.csv","r")
-    li=[]
-    for item  in  f:
-        item=item.strip()
-        li.append(item)
-    f.close()
-    fields=li[0]
+   def csv_to_json(self):
+    temp_storage=[]
+    with File('input.csv', 'r') as opened_file:
+        for item  in  opened_file:
+            item=item.strip()
+            temp_storage.append(item)
+    fields=temp_storage[0]
     fields=fields.split(";")
-    li.remove(li[0])
+    temp_storage.remove(temp_storage[0])
     data=[]
-    for item  in li:
-      x={}
+    for item  in temp_storage:
+      new_object={}
       item=item.split(";")
       for i  in  range(0,len(fields)-1):
           if item[i]!=None:
               if item[i].find(",")==-1:
-                 x[fields[i]]=item[i]
+                 new_object[fields[i]]=item[i]
               else:
-                  x[fields[i]]=item[i].split(",")
+                  new_object[fields[i]]=item[i].split(",")
           else:
-               x[fields[i]]=''
-      data.append(x)
-    y={}
-    y["data"]=data
-    f1=open("output.json","w")
-    f1.write(json.dumps(y))
-    f1.close()
+               new_object[fields[i]]=''
+      data.append(new_object)
+    final_object={}
+    final_object["data"]=data
+    with File("output.json","w") as  opened_file:
+        opened_file.write(json.dumps(final_object))
+  
      
-   def jsontocsv(self):
-    f1=open("input.json","r")
-    data=f1.read()
-    f1.close()
-    obj=json.loads(data)
-    self.writefields(obj["data"][0])
-    for  item in obj["data"]:
-        self.writer(item)
+   def json_to_csv(self):
+        with  File("input.json","r")  as  opened_file:
+            data=opened_file.read()
+        parsed_object=json.loads(data)
+        self.write_fields(parsed_object["data"][0])
+        for  item in parsed_object["data"]:
+            self.writer(item)
 
-   def  writefields(self,obj):
+   def  write_fields(self,field_object):
         columns=""
-        for keys  in obj.keys():
+        for keys  in field_object.keys():
             columns=columns+keys+";"
-        f=open("output.csv","w")
-        f.write(columns)
-        f.write("\n")
-        f.close()
-   def  writer(self,obj):
+        with  File("output.csv","w") as  opened_file:
+            opened_file.write(columns)
+            opened_file.write("\n")
+   def  writer(self,csv_item):
         data=""
-        for item in  obj.values():
+        for item in  csv_item.values():
             if type(item)==list:
-               x=""
-               for  it  in  item:
-                   x=x+str(it)+","
-               data=data+x[:-1]+";"
+               temp_storage=""
+               for  inner_item  in  item:
+                   temp_storage=temp_storage+str(inner_item)+","
+               data=data+temp_storage[:-1]+";"
             else:
                 data=data+str(item)+";"
-      
-        f=open("output.csv","a")
-        f.write(data)
-        f.write("\n")
-        f.close()
+        with File("output.csv","a")  as  opened_file:
+            opened_file.write(data)
+            opened_file.write("\n")
 
    
 
 
-
-x=Converter()
-x.csvtojson()
-x.jsontocsv()
-
-
+converter=Converter()
+converter.json_to_csv()
+converter.csv_to_json()
