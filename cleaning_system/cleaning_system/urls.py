cab83583ff7a41956771efcc2e7de8a82549bcbb
Temurbekjonbtz <56353734+Temurbekjonbtz@users.noreply.github.com>
2022-05-19 17:08:10 +0500

    homework  7  done  using jwt authentication

--- cleaning_system/cleaning_system/urls.py
+++ cleaning_system/cleaning_system/urls.py
@@ -18,6 +18,8 @@ from django.urls import path,  include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('',include('cleaning_company.urls')),
     path('',include('rating_review.urls')),
     path('',include('service_type.urls')),
